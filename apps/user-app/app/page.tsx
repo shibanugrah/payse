2d3a7939896f300f9fdbbf7335f273bc52e029af
shibanugrah <shibanugrahap40@gmail.com>
2024-11-09 18:51:21 +0530

    Made changes to the user-app/page.tsx & added Appbar there.

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-import {useBalance} from "@repo/store/useBalance"
+import { signIn, signOut, useSession } from "next-auth/react"
+import {Appbar} from "@repo/ui/Appbar"
 
-export default function() {
-  const balance = useBalance()
+export default function Page(): JSX.Element {
+  const session = useSession();
   return <div>
-    Hey there {balance}
+    <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user}></Appbar>
   </div>
 }
