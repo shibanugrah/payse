bad759a85d8ffa64c0a64dff30d7ac9f46a06a8a
shibanugrah <shibanugrahap40@gmail.com>
2024-11-10 13:23:05 +0530

    Add transaction to update the balance & transactions DB.

---
+++ apps/bank_webhook_handler/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import db from "@repo/db/client"
+
+const app = express();
+
+app.post("/hdfcWebhook", (req, res) => {
+    //TODO: Add zod validation here?
+    const paymentInformation = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    };
+    // Update balance in db, add txn
+})
